a1ec2ebb788adc71b5b3030c26b4b233f709af64
Syed Umair <umair_developer@outlook.com>
2020-11-30 06:15:59 +0530

    Project Refactored and Completed

--- main.py
+++ main.py
@@ -7,30 +7,32 @@ from email.message import EmailMessage
 from datetime import datetime
 
 print("Starting Automated Testing")
-# username = input("Enter The Username Of Knowledge Pro\n")
-# password = input("Enter The Password Of Knowledge Pro\n")
-username = "20181CSE0734"
-password = 48793895
+username = input("Enter The Username Of Knowledge Pro\n")
+password = input("Enter The Password Of Knowledge Pro\n")
+recipient = input("Enter Email To Send Alerts\n")
 driver = webdriver.Chrome(ChromeDriverManager().install())
+#Automated Selenium Testing
 driver.get('http://www.google.com/')
 search_box = driver.find_element_by_name('q')
 search_box.send_keys('Presidency Knowledge Pro')
 search_box.submit()
 knowledgepro = driver.find_element_by_xpath('//*[@id="rso"]/div[1]/div/div[1]/a/h3/span')
 knowledgepro.click()
+time.sleep(3)
 usernameinput = driver.find_element_by_id('username')
 passwordinput = driver.find_element_by_id('password')
 usernameinput.send_keys(username)
 passwordinput.send_keys(password)
 login = driver.find_element_by_id("Login")
 login.click()
+print("Successfully Logged Into Knowledge Pro\n")
 sidebar = driver.find_element_by_id("sidebar_main_toggle")
 sidebar.click()
 attendancePage = driver.find_element_by_xpath('//*[@id="sidebar_main"]/div/div[1]/div[2]/ul/li[1]/a')
 attendancePage.click()
 page_source = driver.page_source
 driver.quit()
-# BEAUTIFUL SOUP
+# BEAUTIFUL SOUP Extraction
 soup = BeautifulSoup(page_source, features='lxml')
 data = []
 table = soup.find("table")
@@ -40,12 +42,18 @@ for row in rows:
     cols = row.findAll("td")
     cols = [ele.text.strip() for ele in cols]
     data.append([ele for ele in cols if ele])
+print("Uncleaned Data received")
+print("Data : ")
 print(data)
+print("\n")
+time.sleep(3)
 dataClean1 = list(filter(None, data))  # Remove Empty List
 dataClean2 = dataClean1[2:-3]  # Remove First Unnecessary and Last Unnecessary Row
 lessAttendance = {}
 borderAttendance = {}
 goodAttendance = {}
+print("\nData Cleaned Successfully\n")
+time.sleep(2)
 for subject in dataClean2:
     try:
         first = int(subject[0])
@@ -74,14 +82,17 @@ goodString = ""
 for subject, marks in goodAttendance.items():
     temp = str(subject) + " : " + str(marks)
     goodString = goodString + temp
-
+print("\nLow Attendance And Border Attendance Subjects Obtained\n")
+time.sleep(2)
 email = "201810100741@presidencyuniversity.in"
 password = "Uzair2005"
-recipient = "umairrsyedd.study@gmail.com"
+print("Initiating Email Process")
+time.sleep(2)
 # Initiating Email
 smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
 smtpObj.ehlo()
 smtpObj.starttls()
+print("Email Id Setup Complete")
 msg = EmailMessage()
 msg.set_content("The Following Courses Have VERY LOW ATTENDANCE : %s\n\nThese Courses Are In The Border Line : %s " % (
     leastString, borderString))
@@ -91,5 +102,8 @@ msg['Subject'] = 'Attendance Alerts Auto Generated [%s]' % current_time
 msg['From'] = email
 msg['To'] = recipient
 smtpObj.login(email, password)
+# Sending Email To User
 smtpObj.send_message(msg)
+print("\n\nEmail Sent To User : %s\n" % recipient)
 smtpObj.quit()
+# End Of Program
